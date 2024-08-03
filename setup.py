17b8bf0e37b5999218d9abdf5bf49068f644da66
Tanmay Rastogi <tanmayrastogi57@gmail.com>
2024-08-03 22:39:05 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Voice Assistant',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Tanmay Rastogi',
+    author_email='tanmayrastogi57@gmail.com',
+    description=''
+)
